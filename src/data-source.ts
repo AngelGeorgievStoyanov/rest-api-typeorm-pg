29608602552f5ddb99c0a001d84104e9413d979a
Angel <angelo_stoyanov@abv.bg>
2024-05-31 19:29:29 +0300

    Added dependencies, configured TypeORM with PostgreSQL

---
+++ src/data-source.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm";
+
+// Database connection configuration variables.
+// In production environment, these values should be set via .env file.
+const TYPE = "postgres";
+const USERNAME = "postgres"; // Database user name
+const HOST = "localhost"; // Database host
+const DB = "typeorm-pg"; // Database name
+const PASSWORD = "1234"; // Database connection password
+const PORTDB = 5432; // Database connection port
+// Note: In production environment, the above configuration variables should be
+// moved to a .env file to ensure data security and ease of management.
+
+export const AppDataSource = new DataSource({
+  type: TYPE,
+  host: HOST,
+  port: PORTDB,
+  username: USERNAME,
+  password: PASSWORD,
+  database: DB,
+  synchronize: true,
+  logging: true,
+  entities: [],
+  subscribers: [],
+  migrations: [],
+});
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import "reflect-metadata";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { AppDataSource } from "./data-source";
+
+const app = express();
+const PORT = 8080;
+
+const allowedOrigins = ["http://localhost:5173"];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+  methods: "GET,POST,PUT,DELETE",
+};
+app.use(cors(options));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 100000,
+  })
+);
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.raw({ limit: "50mb", inflate: true }));
+
+
+AppDataSource.initialize()
+  .then(async () => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log("Server error:", error));
